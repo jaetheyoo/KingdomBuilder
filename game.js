aec2ae66173f91132522e3bee573ef558510640d
unknown <fromjae2yoo@gmail.com>
2018-07-15 16:07:41 -0400

    added configs for villages

--- game.js
+++ game.js
@@ -10,16 +10,20 @@ var init = function initVillage(Villages, debug) {
         let village1_room = Game.spawns[village1_Spawns[0]].room;
         let village1_controller = village1_room.controller; 
 
-        //let village2_Spawn = Game.spawns['Spawn1'];
-        //let village2_room = village1_Spawn.room;
-        //let village2_controller = village1_room.controller; 
+        let village2_Spawns =['Spawn2'];
+        let village2_room = Game.spawns[village2_Spawns[0]].room;
+        let village2_controller = village2_room.controller; 
+
+        let village3_Spawns =['Spawn4'];
+        let village3_room = Game.spawns[village3_Spawns[0]].room;
+        let village3_controller = village3_room.controller; 
 
         Memory.Villages = {
             "village1": {
                 "spawns": village1_Spawns,
                 "room": village1_room.name,
                 "controller": village1_controller.id,
-                "level": 1,
+                "level": 5,
                 "remoteRooms": {
                     "W55N48": {
                         "remoteRepairer": 0,
@@ -55,19 +59,83 @@ var init = function initVillage(Villages, debug) {
                 },
                 "structures": {
                     "links" : {
-                        "toLinks": [],
-                        "fromLinks": []
+                        "toLinks": ['5b3a73a00baf6d03ce461043'],
+                        "fromLinks": ['5b3a78fb0baf6d03ce4612bb','5b3da21255276f03b8cf406d']
                     },
-                    "towers" : {}
+                    "towers" : {
+                        '5b48b14a26fe4949defaead8': {},
+                        '5b3981e6cb0b34591b3ef9b7': {},
+                        '5b35dcb7ca59fe44212e6cb7': {}
+                    }
                 }
 
             },
-            //"village2": {
-            //    "spawn": village2_Spawn.name,
-            //    "room": village2_room.name,
-            //    "controller": village2_controller.id,
-            //    "level":1
-            //}
+            "village2": {
+                "spawns": village2_Spawns,
+                "room": village2_room.name,
+                "controller": village2_controller.id,
+                "level": 2,
+                "remoteRooms": {
+                    "W53N49": {
+                        "remoteRepairer": 0,
+                        "remoteSources": {
+                            "59f19fad82100e1594f3563c" : {
+                                remoteDropHarvester: 0, remoteTransporter: 0
+                            },
+                            "59f19fad82100e1594f3563d" : {
+                                remoteDropHarvester: 0, remoteTransporter: 0
+                            }
+                        },
+                        "remoteController": '59f19fad82100e1594f3563b',
+                    },
+                },
+                "flags": {
+                    "hideoutFlag" : 'Room2Spawn',
+                    "refuelWaitingZone" : 'S2U'
+                },
+                "structures": {
+                    "links" : {
+                        "toLinks": ['5b479e0de18cc24347f4a092'],
+                        "fromLinks": ['5b47a7f2b73bda6833af2d87']
+                    },
+                    "towers" : {
+                        '5b479522838e6268321346a4': {},
+                        '5b415d9ca8f9805e72b8b1aa': {}
+                    }
+                }
+            },
+            "village3": {
+                "spawns": village3_Spawns,
+                "room": village3_room.name,
+                "controller": village3_controller.id,
+                "level": 1,
+                "remoteRooms": {
+                    "W52N45": {
+                        "remoteRepairer": 0,
+                        "remoteSources": {
+                            "59f19fbc82100e1594f357d1" : {
+                                remoteDropHarvester: 0, remoteTransporter: 0
+                            },
+                            "59f19fbc82100e1594f357d0" : {
+                                remoteDropHarvester: 0, remoteTransporter: 0
+                            }
+                        },
+                        "remoteController": '59f19fbc82100e1594f357d2',
+                    },
+                },
+                "flags": {
+                    "hideoutFlag" : 'v3Hideout',
+                    "refuelWaitingZone" : 'v3refuel'
+                },
+                "structures": {
+                    "links" : {
+                        "toLinks": [],
+                        "fromLinks": []
+                    },
+                    "towers" : {
+                    }
+                }
+            }
         };
     } 
     
@@ -75,9 +143,12 @@ var init = function initVillage(Villages, debug) {
     
     // if recompiled, create new Village objects reading from memory
     var village1 = new Village("village1",Memory.Villages.village1.room,Memory.Villages.village1.spawns,Memory.Villages.village1.controller,  debug);
-    //var village2 = new Village("village2",Memory.Villages.village2.room,Memory.Villages.village2.spawn,Memory.Villages.village2.controller, debug);
+    var village2 = new Village("village2",Memory.Villages.village2.room,Memory.Villages.village2.spawns,Memory.Villages.village2.controller, debug);
+    var village2 = new Village("village3",Memory.Villages.village3.room,Memory.Villages.village3.spawns,Memory.Villages.village3.controller, debug);
+
     Villages.village1 = village1;
-    //Villages.village2 = village2;
+    Villages.village2 = village2;
+    Villages.village3 = village3;
 }
 
 var upkeep = function upkeep(Villages) {
